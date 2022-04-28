515f0b503897c6b6f8b892e3d5c08d6eda98ad5c
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-04-28 08:56:49 +0200

    Init Breeze, add Item model and seed database with items from csv

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Models\Item;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +19,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $TestUser = new User();
+        $TestUser->name = "mikederycke";
+        $TestUser->email = "mike.derycke@ehb.be";
+        $TestUser->password = Hash::make('backendisawesome');
+        $TestUser->role = "admin";
+        $TestUser->save();
+
+        $csv = fopen(base_path("database/data/medialab_data.csv"), "r");
+        $firstline = true;
+        while (($data = fgetcsv($csv, 2000, ";")) !== FALSE) {
+            if (!$firstline) {
+                print_r($data);
+                $item = new Item();
+                $item->brand = $data['0'];
+                $item->model = $data['1'];
+                $item->category = $data['2'];
+                $item->description = $data['3'];
+                $item->save();
+            }
+            $firstline = false;
+        }
+
+        fclose($csv);
     }
 }
